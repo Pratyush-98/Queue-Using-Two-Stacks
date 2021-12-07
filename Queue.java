d480a2ef591c0adec2435143f839960567cc1235
Pratyush-98 <pratyushsharma899@msitprogram.net>
2021-12-07 21:35:53 +0530

    completed queue function

--- Queue.java
+++ Queue.java
@@ -5,10 +5,12 @@ import java.io.*;
 import java.util.*;
 
 class Queue {
-
-
-    public Queue() {
-
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+    public Queue() 
+    {
+    stack1= new Stack<Integer>();
+    stack2 = new  Stack<Integer>();
     }
 
     // Push element x to the back of queue.
